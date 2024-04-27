dbd9d0b525d99c806381d2ea97b2d742091a877c
Heinemann <Mark.Heinemann@computacenter.com>
2024-04-27 16:26:49 +0100

    initial

---
+++ aci.tf
@@ -0,0 +1,14 @@
+provider "aci" {
+  username = "admin"
+  password = "!v3G@!4@Y"
+  url      = "https://sandboxapicdc.cisco.com/"
+  insecure = true
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-state-markh"
+    region = "us-east-1"
+    # Environment variable for key is AWS_BUCKET_KEY_NAME
+  }
+}
