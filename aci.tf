73c80db4fff5f6b3e83bd90914c71364bd7b603e
Heinemann <Mark.Heinemann@computacenter.com>
2024-04-27 17:20:18 +0100

    add test comment

--- aci.tf
+++ aci.tf
@@ -20,6 +20,7 @@ terraform {
   backend "s3" {
     bucket = "tf-state-markh"
     region = "us-east-1"
+    key = "terraform.tfstate"
     # Environment variable for key is AWS_BUCKET_KEY_NAME
   }
 }
